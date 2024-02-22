965a7a4cad34b396b548e8a68c82b05bb469c19a
Lyrafaiqahh <lyrafaiqah@gmail.com>
2024-02-22 13:30:20 +0700

    Percobaan1 Lyra

---
+++ Buku15.java
@@ -0,0 +1,37 @@
+
+
+public class Buku15 {
+   
+    
+   
+     String judul, pengarang;
+     int halaman, stok, harga;
+
+     void tampilInformasi (){
+        System.out.println("Judul:" + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+     }
+        
+     void terjual(int jml) {
+    if (stok > 0) {
+        stok -= jml;
+        if (stok <= 0) {
+            stok = 0; 
+        }
+        System.out.println("Berhasil!");
+    } else {
+        System.out.println("Stok tidak tersedia.");
+    }
+}
+
+     void restock (int jml){
+        stok += jml;
+     }
+
+     void gantiHarga (int hrg) {
+        harga = hrg;
+     }
+    }
